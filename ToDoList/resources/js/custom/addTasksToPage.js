f8ac8bfaf01b22a43ba1e1a5f011588df4758ad5
Emilien Charpié <pk72omy@eduvaud.ch>
2022-06-01 11:40:47 +0200

    Ajout des tâches de manière dynamique dans les groupes

--- ToDoList/resources/js/custom/addTasksToPage.js
+++ ToDoList/resources/js/custom/addTasksToPage.js
@@ -1,15 +1,51 @@
 $.get("../../../ToDoList/ToDoList/data/tasks.json", function(data) {
     let tasks = new Array(data.length)
 
+    //Get all the tasks
     for (let i = 0; i < data.length; i++) {
         tasks[i] = data[i]
+    }
+
+    var URLvalues = new Array();
+    let groupName;
+    URLvalues = orderSearch();
+    groupName = GetGroupName(URLvalues);
+
+    function orderSearch() {        
+        let searchUrl = window.location.search;
+        //Get the group where is the user
+        var search = searchUrl.substr(1);
+        //Gets the elements of the serach separately in an array
+        //(ex: gets vaut : [controller=home], [action=home])
+        var gets = search.split('&');
+        var values = new Array();
+    
+        //Get the values and the name in an array 
+        //(ex: si "element" égal : "controller=home", l'index [0] va valoir "controller" et l'index [1] va valoir "home")
+        gets.forEach(element => {
+            var tempGetsValues = element.split('=');
+    
+            values.push(tempGetsValues[0]);
+            values.push(tempGetsValues[1]);
+        });
+        return values;
+    }
+    function GetGroupName(valuesOfGet) {
+        
+        //Get the value off the get "os" or of the get "constructor"
+        for (let i = 0; i < valuesOfGet.length; i++) {
+            if(valuesOfGet[i] == "group"){
+                return valuesOfGet[i + 1];
+            }
+        }
+        return "";
     }
 
     //Filter tasks from gorups
     let tasksFiltred = new Array(tasks.length);
     for (let ii = 0; ii < tasks.length; ii++) {
         let tabindex = 0;
-        if(tasks[ii]['group'] == "anglais"){
+        if(tasks[ii]['group'] == groupName){
             tasksFiltred[tabindex] = tasks[ii]
             tabindex += 1;
         }
